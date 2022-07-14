9285b091159f43480e010ecd2414ac620118de37
Charles Trenholm <charleshtrenholm@gmail.com>
2022-07-14 08:03:21 -0700

    query hacking investigation round 2 - adds successful rtk query for static service

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,22 +1,38 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../app/store';
 
 export const wsObjectApi = createApi({
   reducerPath: 'wsObjectApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://ci.kbase.us/services/ws' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://ci.kbase.us/services/ws',
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (token) {
+        headers.set('authorization', `${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getwsObjectByName: builder.query<any, string>({
+      keepUnusedDataFor: 600,
       query: (upa) => ({
         url: '',
         method: 'POST',
         body: {
           version: '1.1',
           method: 'Workspace.get_objects2',
-          id: '42',
-          params: [{ objects: [{ ref: '67470/1/6' }] }],
+          id: Math.random(),
+          params: [{ objects: [{ ref: upa }] }],
         },
       }),
     }),
   }),
-})
+  // TODO: figure out how it works with DynamicServiceClient
+  // TODO: figure out how it works with handling JSONRPCErrors
+  // TODO: investigate mutations
+});
 
 export const { useGetwsObjectByNameQuery } = wsObjectApi;
